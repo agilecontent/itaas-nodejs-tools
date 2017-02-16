ba8566b9b5178beb470e2f9a335955beecf7b57d
Diego Sandrim <diego.sandrim@agilecontent.com>
2017-02-16 09:47:19 -0200

    Temp commit

---
+++ test/unit/cmd/license-builder-test.js
@@ -0,0 +1,108 @@
+'use strict';
+/* global describe,it */
+
+const should = require('should'); // eslint-disable-line no-unused-vars
+const licenseBuilder = require('../../lib/cmd/license-builder');
+const fs = require('fs');
+
+let tempFile = 'temp-license-file.tmp';
+
+describe('LicenseBuilder', function () {
+  before(function () {
+    if (fs.existsSync(tempFile)) { fs.unlinkSync(tempFile); };
+  });
+
+  after(function () {
+    if (fs.existsSync(tempFile)) { fs.unlinkSync(tempFile); };
+  });
+
+  describe('.build', function () {
+    it('writes to already existes', function () {
+      tools.date.isDate('2016-06-24 23:56').should.be.equal(true);
+      tools.date.isDate('2016-06-24 23:56Z').should.be.equal(true);
+      tools.date.isDate('2016-06-24 23:56+1000').should.be.equal(true);
+      tools.date.isDate('2016-06-24 23:56:10Z').should.be.equal(true);
+      tools.date.isDate('2016-06-24 23:56:10+1000').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56Z').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56+1000').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56:10').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56:10Z').should.be.equal(true);
+      tools.date.isDate('2016-06-24T23:56:10+1000').should.be.equal(true);
+      tools.date.isDate('2016-06-24').should.be.equal(true);
+    });
+
+    it('should not validate', function () {
+      tools.date.isDate(undefined).should.be.equal(false);
+      tools.date.isDate(null).should.be.equal(false);
+      tools.date.isDate(123).should.be.equal(false);
+      tools.date.isDate('123').should.be.equal(false);
+      tools.date.isDate('2016-24-06').should.be.equal(false);
+      tools.date.isDate('24-06-2016').should.be.equal(false);
+      tools.date.isDate('06-24-2016').should.be.equal(false);
+    });
+  });
+
+  describe('.parseDate', function () {
+    it('should not convert undefined value', function () {
+      (() => tools.date.parseDate(undefined)).should.throw();
+      (() => tools.date.parseDate(null)).should.throw();
+      (() => tools.date.parseDate(123)).should.throw();
+      (() => tools.date.parseDate('123')).should.throw();
+    });
+
+    it('should convert correctly', function () {
+      let date;
+      let dateAsTimestamp;
+
+      date = '2016-06-24 23:56';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24 23:56Z';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24 23:56+1000';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24 23:56:10Z';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24 23:56:10+1000';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56Z';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56+1000';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56:10';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56:10Z';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24T23:56:10+1000';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+      date = '2016-06-24';
+      dateAsTimestamp = moment(date, moment.ISO_8601).toDate().getTime();
+      tools.date.parseDate(date).getTime().should.be.equal(dateAsTimestamp);
+
+    });
+  });
+});
