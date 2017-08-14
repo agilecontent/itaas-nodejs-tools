80a314c8951f6262e65dd8b85671791577097328
Anderson Martins <anderson.martins@uux.com>
2017-08-14 20:23:03 -0300

    Update tests.

--- test/unit/remote-config-tests.js
+++ test/unit/remote-config-tests.js
@@ -6,6 +6,7 @@ const should = require('should');
 const RemoteConfig = require('../../lib/remote-config');
 const uuid = require('uuid').v4;
 const tools = require('../../lib/index');
+const sleep = require('sleep');
 
 let callId = uuid();
 let config = { key: 'value' };
@@ -17,7 +18,7 @@ const configServerUrl = 'http://remote.config';
 const configUrl = 'http://remote.config/remoteconfig.json';
 const notFoundConfigUrl = 'http://remote.config/notfound.json';
 const invalidConfigUrl = 'http://config.invalid/invalid.json';
-const configRefreshTime = 5;
+const configRefreshTime = 2;
 
 const configObject = {
   result : {
@@ -34,22 +35,46 @@ describe('Remote Config', function () {
     before(function() {
       nock(configServerUrl)
         .get('/remoteconfig.json')
-        .delay(500)
+        .delay(200)
         .reply(200, configObject);
 
       nock(configServerUrl)
         .get('/notfound.json')
-        .delay(500)
+        .delay(200)
         .reply(404, 'Not Found');        
     });
 
     it('should return config object', function (done) {
+      this.timeout(10000);
       let remoteConfig = new RemoteConfig(configUrl, configRefreshTime);
 
       remoteConfig.getConfigObject(context)
-      .then((result)=>{
+      .then((result)=> {
         should.deepEqual(result, configObject);
-        done();  
+        configObject.result.key5 = 'newvalue';
+
+        let chached = () => { 
+          return remoteConfig.getConfigObject(context); 
+        };  
+
+        let notChached = () => { 
+          sleep.sleep((configRefreshTime + 1));
+
+          nock(configServerUrl)
+           .get('/remoteconfig.json')
+           .delay(200)
+           .reply(200, configObject);
+
+          return remoteConfig.getConfigObject(context); 
+        };  
+        
+        return Promise.all([chached(), notChached()]);
+      })
+      .then((results)=>{
+        should.equal(configObject.result.key5,'newvalue');
+        should.notDeepEqual(results[0], configObject);
+        should.deepEqual(results[1], configObject);
+        done();          
       })
       .catch(done);
     });
