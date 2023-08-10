ab9a1dbea3dc198211453c05439f3b6edcd8fa81
Graziela Madella Kranz <garzehella@MacBook-Air-de-Graziela.local>
2023-08-10 18:43:40 -0300

    fix script

--- scripts/travis-step-after-deploy.sh
+++ scripts/travis-step-after-deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 APP_VERSION=$(cat package.json | grep -Po 'version\"\:[ ]*\"\K[0-9.]+')
+REPOSITORY_NAME="itaas-nodejs-tools"
 
-if [ "${TRAVIS_EVENT_TYPE:-}" == "pull_request" ] 
-then
-  JIRA_KEY_NUMBER=$(echo $TRAVIS_PULL_REQUEST_BRANCH| sed 's/refs\/heads\///g' | grep -ioP '[[:digit:]]{4,}' | head -1)
+if [ -z $PR_BRANCH ]; then
+    DESCRIPTION=""
+else
+  JIRA_KEY_NUMBER=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed 's/refs\/heads\///g' | grep -ioP '[[:digit:]]{4,}' | head -1)
   DESCRIPTION=$(echo "[ITAAS-$JIRA_KEY_NUMBER](https://jiralabone.atlassian.net/browse/ITAAS-$JIRA_KEY_NUMBER)")
+fi
 
   curl \
     -X POST \
     -H 'Accept: application/vnd.github.v3+json' \
     -H "Authorization: token $GITHUB_OAUTH_TOKEN" \
-    https://api.github.com/repos/agilecontent/itaas-nodejs-tools/releases \
-    -d '{"tag_name":"'$TRAVIS_APP_VERSION'", "body":"'$DESCRIPTION'" }'
-else
-  echo Skipping Github Release...
-fi
+    https://api.github.com/repos/agilecontent/$REPOSITORY_NAME/releases \
+    -d '{"tag_name":"'$APP_VERSION'", "body":"'$DESCRIPTION'" }'
